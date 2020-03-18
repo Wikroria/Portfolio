9da927deb36f7e90ee3c36f39639bc42ae0a11ba
wiktoriauli <wiktoria.ulidowska@gmail.com>
2020-03-18 09:50:36 +0100

    Update

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-/*new fullpage('#fullpage',{
-    anchors: ['firstPage'],
-    autoScrolling: true,
-    scrollOverflow: true,
-})*/
-
 var myFullpage = new fullpage('#fullpage', {
-    anchors: ['firstPage', 'secondPage', '3rdPage',],
     scrollOverflow: true,
     navigation: true,
     navigationPosition: 'right',
