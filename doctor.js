3a6ffb992871965acaa2d7e2cdf709f7da5c495f
wiktoriauli <wiktoria.ulidowska@gmail.com>
2020-05-26 22:37:37 +0200

    Adding an ux project-doctor apointment

---
+++ doctor.js
@@ -0,0 +1,20 @@
+let bm = document.getElementsByClassName('bm');
+
+Array.prototype.forEach.call(bm, icon => {
+    let anim = bodymovin.loadAnimation({
+        container: icon,
+        path: `../icons/${icon.dataset.file}.json`,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false, 
+    })
+    icon.addEventListener('mouseenter', () => {
+        anim.setDirection(1)
+        anim.play();
+    })
+    
+    icon.addEventListener('mouseleave', () => {
+        anim.setDirection(-1)
+        anim.play();
+    })
+})
--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-lottie.loadAnimation({
-    container: document.getElementById('git'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'icons/${icon.dataset.file}.json'
-  });
+let bm = document.getElementsByClassName('bm');
+
+Array.prototype.forEach.call(bm, icon => {
+    let anim = bodymovin.loadAnimation({
+        container: icon,
+        path: `icons/${icon.dataset.file}.json`,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false, 
+    })
+    icon.addEventListener('mouseenter', () => {
+        anim.setDirection(1)
+        anim.play();
+    })
+    
+    icon.addEventListener('mouseleave', () => {
+        anim.setDirection(-1)
+        anim.play();
+    })
+})
+
+// co zrobić żeby przy mobilnej wersji po kliknieciu ikona wracała do stanu 0 
+
+
+
+ 
+
